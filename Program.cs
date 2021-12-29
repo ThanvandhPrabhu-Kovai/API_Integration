262052b3dd9c1041ec63d51f982b162b2f2190c3
thanvandh4435 <thanvandh4435@gmail.com>
2021-12-29 17:24:45 +0530

    API_Integration_Proj - Initial Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+
+namespace API_Integration
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            getResults();
+            Console.ReadLine();
+        }
+
+        static async void getResults() {
+            string result = await client.GetStringAsync("https://api.openweathermap.org/data/2.5/weather?q=Coimbatore&appid=5d6edcfd015c6df6d879f1bca2fc2344");
+            Console.Write(result);
+        }
+    }
+}
